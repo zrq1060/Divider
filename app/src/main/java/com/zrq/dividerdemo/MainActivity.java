1d1cf4873ec8f8215fe27bca32886a6165425419
zhangrq <zrq1060@163.com>
2018-04-20 10:43:09 +0800

    功能完善

--- app/src/main/java/com/zrq/dividerdemo/MainActivity.java
+++ app/src/main/java/com/zrq/dividerdemo/MainActivity.java
@@ -1,13 +1,225 @@
 package com.zrq.dividerdemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.zrq.divider.Divider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inputNumEditText;
+    private RecyclerView recyclerView;
+    private TestAdapter adapter;
+    private RecyclerView.LayoutManager linearLayoutManager;
+    private List<View> headerViews = new ArrayList<>();
+    private List<View> footerViews = new ArrayList<>();
+    private Divider divider;
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputNumEditText = findViewById(R.id.et_input_num);
+        recyclerView = findViewById(R.id.recyclerView);
+        // 初始化监听
+        initListener();
+        // 设置adapter
+        adapter = new TestAdapter(R.layout.item_test, null);
+        recyclerView.setAdapter(adapter);
+        divider = Divider.builder().color(Color.RED).width(5).height(5).headerCount(0).footerCount(0).build();
+        recyclerView.addItemDecoration(divider);
+        // 设置manager
+        switchLayoutManager(5);
+        // 设置数据
+        replaceData(10);
+    }
+
+    private void initListener() {
+        // 文字改变监听
+        inputNumEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String trim = inputNumEditText.getText().toString().trim();
+                if (TextUtils.isEmpty(trim))
+                    return;
+                num = Integer.parseInt(trim);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        // 设置内容数量
+        findViewById(R.id.btn_set_num).setOnClickListener(v -> replaceData(num));
+        // 增加内容数量
+        findViewById(R.id.btn_add_num).setOnClickListener(v -> addData(num));
+        // 删除内容数量
+        findViewById(R.id.btn_remove_num).setOnClickListener(v -> removeData(num));
+        // 增加头
+        findViewById(R.id.btn_header_add).setOnClickListener(v -> {
+            View headerView = getHeaderViewOrFooterView();
+            adapter.addHeaderView(headerView);
+            headerViews.add(headerView);
+            divider.setHeaderCount(adapter.getHeaderLayoutCount());
+            redrawDivider();// 增删得重新的绘制线
+        });
+        // 移除头
+        findViewById(R.id.btn_header_remove).setOnClickListener(v -> {
+            if (headerViews.size() > 0) {
+                adapter.removeHeaderView(headerViews.get(0));
+                headerViews.remove(0);
+                divider.setHeaderCount(adapter.getHeaderLayoutCount());
+                redrawDivider();// 增删得重新的绘制线
+            }
+        });
+        // 增加尾
+        findViewById(R.id.btn_footer_add).setOnClickListener(v -> {
+            View headerView = getHeaderViewOrFooterView();
+            adapter.addFooterView(headerView);
+            footerViews.add(headerView);
+            divider.setFooterCount(adapter.getFooterLayoutCount());
+            redrawDivider();// 增删得重新的绘制线
+        });
+        // 移除尾
+        findViewById(R.id.btn_footer_remove).setOnClickListener(v -> {
+            if (footerViews.size() > 0) {
+                adapter.removeFooterView(footerViews.get(0));
+                footerViews.remove(0);
+                divider.setFooterCount(adapter.getFooterLayoutCount());
+                redrawDivider();// 增删得重新的绘制线
+            }
+        });
+        // 切换LayoutManager
+        findViewById(R.id.btn_linear_v).setOnClickListener(v -> switchLayoutManager(1));
+        findViewById(R.id.btn_linear_h).setOnClickListener(v -> switchLayoutManager(2));
+        findViewById(R.id.btn_grid_v).setOnClickListener(v -> switchLayoutManager(3));
+        findViewById(R.id.btn_grid_h).setOnClickListener(v -> switchLayoutManager(4));
+        findViewById(R.id.btn_staggered_v).setOnClickListener(v -> switchLayoutManager(5));
+        findViewById(R.id.btn_staggered_h).setOnClickListener(v -> switchLayoutManager(6));
+    }
+
+    /**
+     * 切换LayoutManager
+     */
+    private void switchLayoutManager(int layoutManagerState) {
+        switch (layoutManagerState) {
+            case 1:
+                // LinearLayoutManager VERTICAL
+                linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+                break;
+            case 2:
+                // LinearLayoutManager HORIZONTAL
+                linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+                break;
+            case 3:
+                // GridLayoutManager VERTICAL
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 3, LinearLayoutManager.VERTICAL, false);
+                this.linearLayoutManager = gridLayoutManager;
+                setSpanSizeLookup(gridLayoutManager);// 设置头、尾，占用3列
+                break;
+            case 4:
+                // GridLayoutManager HORIZONTAL
+                GridLayoutManager gridLayoutManagerHorizontal = new GridLayoutManager(getApplicationContext(), 3, LinearLayoutManager.HORIZONTAL, false);
+                this.linearLayoutManager = gridLayoutManagerHorizontal;
+                setSpanSizeLookup(gridLayoutManagerHorizontal);// 设置头、尾，占用3列
+                break;
+            case 5:
+                // StaggeredGridLayoutManager VERTICAL
+                this.linearLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+                break;
+            case 6:
+                // StaggeredGridLayoutManager HORIZONTAL
+                this.linearLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.HORIZONTAL);
+                break;
+        }
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter.setLayoutManagerState(layoutManagerState);
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * GridLayoutManager 设置头、尾，占用3列
+     */
+    private void setSpanSizeLookup(GridLayoutManager gridLayoutManager) {
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position < adapter.getHeaderLayoutCount() || position >= adapter.getItemCount() - adapter.getFooterLayoutCount())
+                    // 头或者尾部，占用3列
+                    return gridLayoutManager.getSpanCount();
+                else
+                    return 1;
+            }
+        });
+    }
+
+    /**
+     * 获取增加的头、尾
+     */
+    private View getHeaderViewOrFooterView() {
+        View headerView = View.inflate(getApplicationContext(), R.layout.item_header_footer, null);
+        headerView.setBackgroundColor(new Random().nextInt());
+        headerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        return headerView;
+    }
+
+    /**
+     * 置换数据
+     */
+    private void replaceData(int itemCount) {
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 0; i < itemCount; i++) {
+            items.add("原始数据" + i);
+        }
+        adapter.replaceData(items);
+    }
+
+    /**
+     * 增数据
+     */
+    private void addData(int position) {
+        if (position <= adapter.getItemCount()) {
+            adapter.addData(position, "新增数据");
+            redrawDivider();// 增删得重新的绘制线
+        }
+    }
+
+    /**
+     * 删数据
+     */
+    private void removeData(int position) {
+        if (position < adapter.getItemCount()) {
+            adapter.remove(position);
+            redrawDivider();// 增删得重新的绘制线
+        }
+    }
+
+    /**
+     * 重新绘制线
+     */
+    private void redrawDivider() {
+        // 如果增删调用的是  adapter.notifyDataSetChanged() 则不用调用下面方法
+        recyclerView.invalidateItemDecorations();
     }
 }
---
+++ app/src/main/java/com/zrq/dividerdemo/TestAdapter.java
@@ -0,0 +1,68 @@
+package com.zrq.dividerdemo;
+
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+/**
+ * 描述:
+ *
+ * @author zhangrq
+ * 2018/3/22 16:18
+ */
+
+public class TestAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+    private int layoutManagerState;
+
+    TestAdapter(int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder holder, String item) {
+        int position = holder.getAdapterPosition();
+        holder.itemView.setBackgroundColor(Color.parseColor("#00ff00"));
+        // 设置Item宽高
+        switch (layoutManagerState) {
+            case 1:
+                // LinearLayoutManager VERTICAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(400, 200));
+                break;
+            case 2:
+                // LinearLayoutManager HORIZONTAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(400, 200));
+                break;
+            case 3:
+                // GridLayoutManager VERTICAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300));
+                break;
+            case 4:
+                // GridLayoutManager HORIZONTAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(300, ViewGroup.LayoutParams.MATCH_PARENT));
+                break;
+            case 5:
+                // StaggeredGridLayoutManager VERTICAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, position % 2 == 0 ? 200 : 500));
+                break;
+            case 6:
+                // StaggeredGridLayoutManager HORIZONTAL
+                holder.itemView.setLayoutParams(new ViewGroup.LayoutParams(position % 2 == 0 ? 200 : 500, ViewGroup.LayoutParams.MATCH_PARENT));
+                break;
+        }
+        // 设置数据
+        holder.setText(R.id.textView, item);
+    }
+
+    public void setLayoutManagerState(int layoutManagerState) {
+        this.layoutManagerState = layoutManagerState;
+    }
+}
